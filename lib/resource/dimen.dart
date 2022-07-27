3ca147d8fe3e9fbe352b8a4a62320cb34264ac41
ext-linhchau <ext-linhchau@spdigital.sg>
2022-07-27 13:40:00 +0700

    complete screen vertify otp

--- lib/resource/dimen.dart
+++ lib/resource/dimen.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'colors_data.dart';
+import 'size_config.dart';
+
 class Dimen {
   //input
   static const double borderInput = 10.0;
   static const double paddingChildInput = 10.0;
+
   /// button
   static const double radiousButton = 10.0;
 
@@ -13,4 +19,19 @@ class Dimen {
   // common
   static const double marginsContent = 15;
 
+}
+final otpInputDecoration = InputDecoration(
+      contentPadding:
+          EdgeInsets.symmetric(vertical: getProportionateScreenWidht(15)),
+      enabledBorder: outLineInputBorder(),
+      focusedBorder: outLineInputBorder(),
+      border: outLineInputBorder(),
+      
+  ); 
+
+OutlineInputBorder outLineInputBorder() {
+  return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15),
+      borderSide:  const BorderSide(color: ColorsData.borderInput),
+  );
 }
--- lib/views/otp/components/body.dart
+++ lib/views/otp/components/body.dart
@@ -1,6 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:structure/resource/dimen.dart';
 import 'package:structure/resource/text_type.dart';
 
 import '../../../resource/size_config.dart';
@@ -42,7 +43,8 @@ class _BodyState extends State<Body> {
                         TweenAnimationBuilder(
                             tween: Tween<double>(begin: 30.0, end: 0),
                             duration: Duration(seconds: 30),
-                            builder: (context, double val, child) => Text("${val.toInt()}"))
+                            builder: (context, double val, child) =>
+                                Text("${val.toInt()}"))
                       ],
                     )
                   ],
@@ -59,17 +61,104 @@ class _BodyState extends State<Body> {
   }
 }
 
-// class PhoneForm extends StatelessWidget {
-//   const PhoneForm({Key? key}) : super(key: key);
+class OTPForm extends StatefulWidget {
+  const OTPForm({Key? key}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Form(
-//       child: Column(
-//         children: [
+  @override
+  State<OTPForm> createState() => _OTPFormState();
+}
 
-//         ],       
-//       ),
-//     )
-//   }
-// }
+class _OTPFormState extends State<OTPForm> {
+  late FocusNode pin2FocusNode;
+  late FocusNode pin3FocusNode;
+  late FocusNode pin4FocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    pin2FocusNode = FocusNode();
+    pin3FocusNode = FocusNode();
+    pin4FocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    pin2FocusNode.dispose();
+    pin3FocusNode.dispose();
+    pin4FocusNode.dispose();
+    super.dispose();
+  }
+
+  void nextField({String? value, FocusNode? focusNode}) {
+    if (value!.length == 1) {
+      focusNode!.requestFocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: getProportionateScreenWidht(60),
+            child: TextFormField(
+              autofocus: true,
+              obscureText: true,
+              keyboardType: TextInputType.number,
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+              decoration: otpInputDecoration,
+              onChanged: (value) {
+                nextField(value: value, focusNode: pin2FocusNode);
+              },
+            ),
+          ),
+          SizedBox(
+            width: getProportionateScreenWidht(60),
+            child: TextFormField(
+              focusNode: pin2FocusNode,
+              obscureText: true,
+              keyboardType: TextInputType.number,
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+              decoration: otpInputDecoration,
+              onChanged: (value) {
+                nextField(value: value, focusNode: pin3FocusNode);
+              },
+            ),
+          ),
+          SizedBox(
+            width: getProportionateScreenWidht(60),
+            child: TextFormField(
+              focusNode: pin3FocusNode,
+              obscureText: true,
+              keyboardType: TextInputType.number,
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+              decoration: otpInputDecoration,
+              onChanged: (value) {
+                nextField(value: value, focusNode: pin4FocusNode);
+              },
+            ),
+          ),
+          SizedBox(
+            width: getProportionateScreenWidht(60),
+            child: TextFormField(
+              obscureText: true,
+              focusNode: pin4FocusNode,
+              keyboardType: TextInputType.number,
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+              decoration: otpInputDecoration,
+              onChanged: (value) {
+                pin4FocusNode.unfocus();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
