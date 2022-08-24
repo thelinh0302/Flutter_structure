e6e482802fe62a5ed3f4fa6a61d59244cd0735b5
ext-linhchau <ext-linhchau@spdigital.sg>
2022-08-24 14:13:56 +0700

    implement screen all feature

--- lib/views/home/widget/widget_feature/widget_all_feature.dart
+++ lib/views/home/widget/widget_feature/widget_all_feature.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:structure/resource/colors_data.dart';
+import 'package:structure/resource/text_type.dart';
+
+import '../../../../data_source/mock_data.dart';
 
 class WidgetAllFeature extends StatefulWidget {
   @override
@@ -8,8 +13,23 @@ class WidgetAllFeature extends StatefulWidget {
 class _WidgetAllFeatureState extends State<WidgetAllFeature> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('feature all'),
-    );
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: ColorsData.appBarBackground,
+          title: Text('Feature Partners', style: TextsStyle.titleAppBar),
+        ),
+        body: GridView.builder(
+            itemCount: 10,
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                mainAxisExtent: (MediaQuery.of(context).size.width / 2) * 1.6,
+                maxCrossAxisExtent: MediaQuery.of(context).size.width / 2),
+            itemBuilder: (context, index) {
+              return Container(
+                color: Colors.black,
+              );
+            }));
   }
 }
